22e9063fda03b04c0d58ddef06cc9365d5d7ce94
OlivierCoq <coq.olivier36@gmail.com>
2020-03-25 14:10:40 -0400

    commiting everything

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Title of Your Website</title>
+
+    <!--Open Graph-->
+    <meta property="og:title" content="What this page is about" />
+    <meta property="og:url" content="https://www.titleofwebsite.com" /><!--URL of current Page-->
+    <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"><!--Mobile-->
+
+    <!-- CSS  -->
+
+  </head>
+  <body>
+    <h1>Let's Go</h1>
+  </body>
+</html> 
