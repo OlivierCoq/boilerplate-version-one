d66b495bf7803343d072ca9e0e7fd45901ec56ba
OlivierCoq <coq.olivier36@gmail.com>
2020-03-26 11:58:11 -0400

    removing unnecessary dependencies

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,37 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
 
-exports.default = defaultTask
+// function defaultTask(cb) {
+//   // place code for your default task here
+//   cb();
+// }
+//
+// exports.default = defaultTask
+
+//Compile Sass
+
+gulp.task('sass', function() {
+
+  return gulp.src(['library/sass/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+});
+
+
+//Watch & Serve
+gulp.task('serve', ['sass'], function(){
+  browserSync.init({
+    server: './src'
+  });
+
+gulp.watch(['library/sass/*.scss'], ['sass']);
+gulp.watch(['src/*.php']).on('change', browserSync.reload);
+
+});
+
+
+//Default task
+
+gulp.task('default', ['serve']);
--- index.php
+++ index.php
@@ -9,8 +9,6 @@
     <meta property="og:url" content="https://www.titleofwebsite.com" /><!--URL of current Page-->
     <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"><!--Mobile-->
 
-    <!-- CSS  -->
-    <link rel="stylesheet" href="../boilerplate_2020.prj/css/master.css">
 
   </head>
   <body>
